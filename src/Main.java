941ab1c0e56827379b514beae8d6d3d973190f56
Orlane2905 <orlanedjuidje748@gmail.com>
2026-05-13 21:46:06 +0200

    Mise à jour Emprunt et Main : calcul amende selon jours de retard

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 import pattern.strategy.Emprunt;
 
 public class Main {
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
